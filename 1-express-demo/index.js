a8db131757bdadf65b0bc68e76a0fa7958b04b6a
richardye101 <richardyeee@outlook.com>
2022-05-11 21:55:27 -0400

    vidly project

--- 1-express-demo/index.js
+++ 1-express-demo/index.js
@@ -26,7 +26,7 @@ app.get('/api/courses/:id', (req,res)=>{
     // .find is built into every JS array
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if(!course) {//return 404 error, result not found
-        res.status(404).send('The course with the given ID was not found');
+        return res.status(404).send('The course with the given ID was not found');
     }
     res.send(course);
 });
@@ -48,14 +48,20 @@ app.post('/api/courses', (req,res)=>{
     //     return;
     // }
 
-    // define Joi schema for our courses
-    const schema = {
-      name: Joi.string().min(3).required()  
-    };
+    // define Joi schema for our courses, refactored to a function
+    // const schema = Joi.object({
+    //   name: Joi.string().min(3).required()  
+    // });
+    // const result = schema.validate(req.body);
+    // console.log(result);
+    // if(result.error){
+    //     res.status(400).send(result.error.details[0].message);
+    // }
+    const { error } = validateCourse(req.body);
 
-    const result = Joi.ValidationError(req.body, schema);
-    console.log(result);
-
+    if(error){
+        return res.status(400).send(error.details[0].message);
+    }
     // Read course obj in body, use properties to create course obj, and add to array
     const course = {
         id: courses.length + 1,
@@ -63,9 +69,65 @@ app.post('/api/courses', (req,res)=>{
     };
     courses.push(course);
     res.send(course);
+
 });
-// app.put();
-// app.delete();
+
+// These are route handlers
+app.put('/api/courses/:id', (req,res)=>{
+    // Look up course
+    // if not exist, return 404 (resource not found)
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) {//return 404 error, result not found
+        return res.status(404).send('The course with the given ID was not found');
+    }
+
+    // if found, validate
+    // not found, 400 (bad request)
+    // We'll reuse this Joi code into a function
+    // const schema = Joi.object({
+    //     name: Joi.string().min(3).required()
+    //   });
+    // const result = schema.validate(req.body);
+
+    // Using the new function to validate
+    // const result = validateCourse(req.body);
+
+    // Object destructuring, as we are only interested in result.error
+    // result has two properties, error and value. we only want error. same as result.error, so we can simply use error
+    const { error } = validateCourse(req.body);
+
+    if(error){
+        return res.status(400).send(error.details[0].message);
+    }
+    // Update course
+    course.name = req.body.name;
+    
+    // Return updated course
+    res.send(course);
+});
+
+// A function to validate a course object from a request thats used in both post and put requests
+function validateCourse(course){
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+      });
+    return schema.validate(course);
+};
+
+app.delete('/api/courses/:id',(req,res) => {
+    // Look up course first
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) {//return 404 error, result not found
+        return res.status(404).send('The course with the given ID was not found');
+    }
+
+    // If found, delete 
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    // Return the deleted course info
+    res.send(course);
+});
 
 // PORT env variable, which is set outside the app
 const port = process.env.PORT || 3000;
---
+++ 1-express-demo/vidly-project/index.js
@@ -0,0 +1,92 @@
+// this service rent movies
+const express = require('express');
+const logger = require('./logger');
+const auth = require('./authentication');
+const Joi = require('joi'); // capitalize because it is a class
+
+const app = express();
+
+// Middleware to parse JSON objects from requests
+// Takes a request object and passes control to another middleware function or returns response to client
+// The arrow functions in the .get functions are middleware functions, as they take in a request object
+app.use(express.json());
+// ^Reads the request, and if there is a JSON object in the body, then it will parse it into a JSON and set req.body
+
+// Custom middleware function
+app.use(logger);
+app.use(auth);
+
+
+// Task 1:
+// create service for managing list of genres
+const genres = [
+    {id:1, "genre":"horror"},
+    {id:2, "genre":"comedy"},
+    {id:3, "genre":"romance"},
+    {id:4, "genre":"action"}
+]
+
+app.get('/', (req,res)=>{
+    res.send("Welcome to my website of movie genres");
+});
+
+// Need endpoint to get all genres
+app.get('/api/genres', (req,res) => {
+    res.send(genres);
+});
+
+app.get('/api/genres/:id', (req,res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send("The requested genre has not been found");
+    res.send(genre);
+});
+
+// Create new genres
+app.post('/api/genres', (req,res)=>{
+    // object destructuring!
+    const { error } = validateGenre(req.body);
+    // 400 is bad request
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const genre = {
+        "id" : genres.length + 1,
+        "genre" : req.body.genre
+    }
+    genres.push(genre);
+    res.send(genre);
+});
+
+// Update genres
+app.put('/api/genres/:id', (req,res)=>{
+    const genreIdx = genres.findIndex(g => g.id === parseInt(req.params.id));
+    if(genreIdx === -1) return res.status(404).send("The requested genre has not been found");
+
+    const { error } = validateGenre(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    genres[genreIdx].genre = req.body.genre;
+    res.send(genres[genreIdx]);
+});
+
+// Delete genres 
+app.delete('/api/genres/:id', (req, res)=>{
+    const genreIdx = genres.findIndex(g => g.id === parseInt(req.params.id));
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    console.log(genre);
+    if(!genre) return res.status(404).send("The requested genres has not been found");
+
+    genres.splice(genreIdx,1);
+    res.send(genre);
+});
+
+function validateGenre(genre){
+    const schema = Joi.object({
+        genre:Joi.string().min(3).required() // only have validation for this
+    });
+    return schema.validate(genre);
+}
+// listen
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (req,res)=>{
+    console.log(`Listening on port ${PORT}...`);
+});
